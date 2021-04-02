6156de598b61aa2b01a53512e50c2373f1821422
Anthony Tantra <tantra.anthony@gmail.com>
2021-04-03 03:39:15 +0800

    updated home page implementation to include search bar and go button

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,30 +1,131 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useLanguage from '../../utils/hooks/useLanguage';
 import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button';
 import Box from '@material-ui/core/Box';
 import { useHistory } from 'react-router-dom';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import { ReactComponent as Logo } from '../../assets/icons/logo.svg';
+import useTheming from '../../utils/hooks/useTheming';
+import Autocomplete, {
+  AutocompleteRenderInputParams,
+} from '@material-ui/lab/Autocomplete';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+
+const styles = makeStyles(() => ({
+  pageContainer: {
+    height: '100vh',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+}));
+
+const initialSuggestions = [
+  'gifs',
+  'holdmybeer',
+  'StartledCats',
+  'Pyongyang',
+  'DotA2',
+];
 
 function Home() {
+  const [subredditName, setSubredditName] = useState<string>('');
+  const [subredditSuggestions, setSubredditSuggestions] = useState<string[]>(
+    initialSuggestions,
+  );
+  const { breakpoints, palette } = useTheming();
+  const isSmDown = useMediaQuery(breakpoints.down('sm'));
+  const classes = styles();
   const { t } = useLanguage();
   const history = useHistory();
 
-  function onButtonPressed() {
-    history.push('/settings');
+  function onGoPressed() {
+    if (!subredditName) {
+      return;
+    }
+
+    history.push(`/r/${subredditName}/hot`);
+  }
+
+  function onGoKeyDown(e: React.KeyboardEvent<HTMLDivElement>) {
+    const { key } = e;
+    // TODO: create enum for this
+    if (key === 'Enter') {
+      onGoPressed();
+    }
+  }
+
+  function onSubredditNameChange(_: any, newValue: string | null) {
+    if (typeof newValue === 'string') {
+      setSubredditName(newValue);
+    }
+  }
+
+  function renderAutocompleteInput(params: AutocompleteRenderInputParams) {
+    return (
+      <TextField
+        {...params}
+        variant="outlined"
+        color="primary"
+        fullWidth
+        placeholder={t('search_subreddit')}
+        onKeyDown={onGoKeyDown}
+      />
+    );
   }
 
   return (
-    <Container maxWidth="lg">
+    <Container
+      className={classes.pageContainer}
+      classes={{ root: classes.pageContainer }}
+      maxWidth="sm"
+    >
       <Box
-        alignItems="center"
         justifyContent="center"
         display="flex"
+        width="100%"
         flexDirection="column"
       >
-        {t('home')}
-        <Button onClick={onButtonPressed} variant="contained" color="primary">
-          {t('settings')}
-        </Button>
+        <Box
+          width="100%"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          marginBottom={2}
+        >
+          <Logo fill={palette.primary.main} width={100} height={100} />
+        </Box>
+        <Box width="100%" display="flex" justifyContent="center">
+          {t('home')}
+        </Box>
+        <Box marginY={2}>
+          <Autocomplete
+            freeSolo
+            inputValue={subredditName}
+            onChange={onSubredditNameChange}
+            onInputChange={onSubredditNameChange}
+            options={subredditSuggestions}
+            renderInput={renderAutocompleteInput}
+          />
+        </Box>
+        <Box
+          width="100%"
+          alignItems="center"
+          display="flex"
+          justifyContent="flex-end"
+        >
+          <Button
+            fullWidth={isSmDown}
+            onClick={onGoPressed}
+            disabled={!subredditName}
+            variant="contained"
+            color="primary"
+          >
+            {t('button.go')}
+          </Button>
+        </Box>
       </Box>
     </Container>
   );
