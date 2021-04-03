b141fc939630239f1549b9a3da5b220c1d222512
Anthony Tantra <tantra.anthony@gmail.com>
2021-04-03 08:38:33 +0800

    removed lodash and added individual lodash debounce, added abstracted debounce function in utils

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,3 +1,9 @@
+import _debounce from 'lodash.debounce';
+
 export function getPicsumImageUri(id: number): string {
   return `https://picsum.photos/id/${id}/200/300`;
+}
+
+export function debounce(func: (...args: any) => any, wait: number) {
+  return _debounce(func, wait);
 }
