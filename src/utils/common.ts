24fdea29f3ee17a86d01ff1a61131f1d5245e954
Anthony Tantra <tantra.anthony@gmail.com>
2021-04-04 13:29:53 +0800

    added utils for formatting date and numbers

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,4 +1,6 @@
 import _debounce from 'lodash.debounce';
+import numeral from 'numeral';
+import dayjs from 'dayjs';
 
 export function getPicsumImageUri(id: number): string {
   return `https://picsum.photos/id/${id}/200/300`;
@@ -6,4 +8,23 @@ export function getPicsumImageUri(id: number): string {
 
 export function debounce(func: (...args: any) => any, wait: number) {
   return _debounce(func, wait);
+}
+
+export function extractSubredditImage(url?: string) {
+  if (url) {
+    return url.split('?')[0];
+  }
+
+  return undefined;
+}
+
+export function formatNumber(number?: number | string, format: string = '0,0') {
+  return numeral(number).format(format);
+}
+
+export function formatDate(
+  date?: number | string,
+  format: string = 'DD MM YYYY',
+) {
+  return dayjs(date).format(format);
 }
